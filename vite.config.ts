fa869fb54ee725011801891fa6c0b67543e67c96
KrepsStyle <kevinnet2000@gmail.com>
2025-12-15 18:52:20 -0300

    Configure GitHub Actions for Node.js build and set base path

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,15 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+
+  build: {
+    // Garante que os caminhos gerados no build sejam relativos ao index.html
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[extname]`,
+      },
+    },
+  },
 })
